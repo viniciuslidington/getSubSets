f99bdba4fea0c46bd7c9de10c5ff062c3510da15
Vinicius Lidington <vlidington@gmail.com>
2025-11-26 17:59:41 -0300

    alt nome de acordo com regra python

---
+++ auxiliar_function.py
@@ -0,0 +1,29 @@
+#funcoes auxiliares para o código
+from my_set_class import MySet
+
+
+def mask_to_subset(mask, elements):
+    subset = MySet()
+    n = len(elements)
+
+    for i in range(n):
+        if mask & (1 << i):  # se o bit i está ligado
+            subset.add(elements[i])
+
+    return subset
+
+def generate_all_subsets(elements):
+    result = MySet()
+    n = len(elements)
+    total_masks = 1 << n  # 2^n
+
+    for mask in range(total_masks):
+        subset = mask_to_subset(mask, elements)
+        result.add(subset)
+
+    return result
+
+
+def getSubSets(A):
+    elements = A.toArray()
+    return generate_all_subsets(elements)
--- main.py
+++ main.py
@@ -1,41 +1,6 @@
 # SubSet question - Encora - Estagio
-from mySetClass import MySet
-
-def mask_to_subset(mask, elements):
-    subset = MySet()
-    n = len(elements)
-
-    for i in range(n):
-        if mask & (1 << i):  # se o bit i está ligado
-            subset.add(elements[i])
-
-    return subset
-
-
-# ============================================================
-# 3) Função auxiliar: gera todos os subsets possíveis
-# ============================================================
-
-def generate_all_subsets(elements):
-    result = MySet()
-    n = len(elements)
-    total_masks = 1 << n  # 2^n
-
-    for mask in range(total_masks):
-        subset = mask_to_subset(mask, elements)
-        result.add(subset)
-
-    return result
-
-
-# ============================================================
-# 4) Função PRINCIPAL getSubSets(A)
-# ============================================================
-
-def getSubSets(A):
-    elements = A.toArray()
-    return generate_all_subsets(elements)
-
+from my_set_class import MySet
+from auxiliar_function import getSubSets 
 
 def main():
 
